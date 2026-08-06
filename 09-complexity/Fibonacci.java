ddf5739ceef6f935f2e9b862d2926c5c2068a9dd
Shyam-202596 <krshyamlal14@gmail.com>
2026-08-06 07:41:38 +0000

    add general formula to find fibonacci number in comlexity folder

--- 09-complexity/Fibonacci.java
+++ 09-complexity/Fibonacci.java
@@ -1,5 +1,14 @@
 public class Fibonacci{
     public static void main(String[] args){
-        System.out.println("Fibo");
+        for(int i = 0; i < 11; i++){
+            System.out.println(fibo(i));
+        }
+    }
+
+    static int fibo(int n){
+        /*
+        direct formula to find any Fibonacci number is: [((1+root(5))/2)^n - ((1-root(5))/2)^n]/root(5)
+        **/
+        return (int)((Math.pow(((1+Math.sqrt(5))/2), n) - Math.pow(((1-Math.sqrt(5))/2), n))/Math.sqrt(5));
     }
 }
