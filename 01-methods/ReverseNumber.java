b93cc6882e9f3c67d7c1e4c8e34de0a462bdf96c
Shyam-202596 <krshyamlal14@gmail.com>
2026-08-07 18:01:14 +0000

    add solution of reverse number in method folder

--- 01-methods/ReverseNumber.java
+++ 01-methods/ReverseNumber.java
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/reverse-integer/?envType=problem-list-v2&envId=egdfqalg
+
 public class ReverseNumber {
     public static void main(String[] args){
 		
@@ -5,27 +7,25 @@ public class ReverseNumber {
 	}
 	
 	static int reverseNumber(int n){
-		if(n == 0){
-			return 0;
-		}
-		
 		int revNum = 0;
-		
-		if(n < 0){
-			n = n * (-1);
-			while(n > 0){
-			revNum = revNum * 10 + n % 10;
+
+		while(n != 0){
+			int digit = n % 10;
 			n = n / 10;
-			}
-			return (-1)*revNum;
+
+			//Integer.MAX_VALUE = 2147483647
+			//Integer.MIN_VALUE = -2147483648
+
+			if(revNum > Integer.MAX_VALUE / 10 || (revNum == Integer.MAX_VALUE / 10 && digit > 7)){
+                return 0;
+            }
+            if(revNum < Integer.MIN_VALUE / 10 ||(revNum == Integer.MIN_VALUE / 10 && digit < -8)){
+                return 0;
+            }
+
+			revNum = revNum * 10 + digit;
 		}
-		
-		else{
-			while(n > 0){
-			revNum = revNum * 10 + n % 10;
-			n = n / 10;
-			}
-		}
+
 		return revNum;
 	}
 }
