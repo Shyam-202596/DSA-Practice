1ef121873befad0ab64d00b65467f8476ad76fe6
Shyam-202596 <krshyamlal14@gmail.com>
2026-09-22 09:44:08 +0000

    add code for Maze problems using recursion

--- 08-recursion/backtracking/MazeProblem.java
+++ 08-recursion/backtracking/MazeProblem.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
+
 public class MazeProblem {
     public static void main(String[] args) throws java.lang.Exception {
         System.out.println(count(0, 0));
         path("", 0, 0);
         System.out.println(pathRet("", 0, 0));
+        System.out.println(pathRetDiagonal("", 0, 0));
+        boolean[][] board = {
+            {true, true, true},
+            {true, false, true},
+            {true, true, true}
+        };
+        pathRestriction("", board, 0, 0);
     }
+
     static int count(int r, int c){
         if(r == 2 || c == 2){
             return 1;
@@ -12,6 +22,7 @@ public class MazeProblem {
         int right = count(r, c + 1);
         return left + right;
     } 
+    
     static void path(String p, int r, int c){
         if(r == 2 && c == 2){
             System.out.println(p);
@@ -24,7 +35,8 @@ public class MazeProblem {
             path(p + 'R', r, c + 1);
         }
     }
-static ArrayList<String> pathRet(String p, int r, int c){
+
+    static ArrayList<String> pathRet(String p, int r, int c){
         if(r == 2 && c == 2){
             ArrayList<String> list = new ArrayList<>();
             list.add(p);
@@ -38,5 +50,40 @@ static ArrayList<String> pathRet(String p, int r, int c){
             ans.addAll(pathRet(p + 'R', r, c + 1));
         }
         return ans;
+    }
+
+    static ArrayList<String> pathRetDiagonal(String p, int r, int c){
+        if(r == 2 && c == 2){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        if(r < 2 && c < 2){
+            ans.addAll(pathRetDiagonal(p + 'D', r + 1, c + 1));
+        }
+        if(r < 2){
+            ans.addAll(pathRetDiagonal(p + 'V', r + 1, c));
+        }
+        if(c < 2){
+            ans.addAll(pathRetDiagonal(p + 'H', r, c + 1));
+        }
+        return ans;
+    }
+
+    static void pathRestriction(String p, boolean[][] maze, int r, int c){
+        if(r == maze.length - 1 && c == maze[0].length - 1){
+            System.out.println(p);
+            return;
+        }
+        if(!maze[r][c]){
+            return;
+        }
+        if(r < maze.length - 1){
+            pathRestriction(p + 'D', maze, r + 1, c);
+        }
+        if(c < maze[0].length - 1){
+            pathRestriction(p + 'R', maze, r, c + 1);
+        }
     }
 }
